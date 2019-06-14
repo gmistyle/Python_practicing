8b8895cd534072cb41de51cef3b21c67c61269b6
Jimmy Lee <realjimmycycle@gmail.com>
2019-06-14 10:14:06 +0800

    Update note_1.py

--- Pandas/note_1.py
+++ Pandas/note_1.py
@@ -9,10 +9,16 @@ data_1 = pd.read_table('path or file name', sep = '|', header = None, names = us
 #the only different is the 'sep'
 data_2 = pd.read_excel('path or file name', sheetname 'sheet_1')
 data_3 = pd.read_csv('path or file name)
+                   
+#show some datas
+data.head()
 
+#descriptive statistics
+data.describe()
+                     
 ###select series
 data.col_1 #it could conflict with some built_in function
-data['col_1']
+data['col_1'] #it would be more
                      
 ###create new series
 data['new_col'] = data['col_1'] + ',' + data['col_2']
